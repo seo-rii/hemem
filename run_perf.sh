edeec3e0a2c37e27e77764527d9bc5ba493cc524
Midhul <midhul.v@gmail.com>
2024-02-26 16:31:58 -0500

    reverted perf path

--- run_perf.sh
+++ run_perf.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-    /home/midhul/linux-6.3/tools/perf/perf stat -e mem_load_l3_miss_retired.local_dram,mem_load_l3_miss_retired.remote_dram -C3,7,11,15,19,23,27,31 &
+    /home/midhul/colloid/linux-6.3/tools/perf/perf stat -e mem_load_l3_miss_retired.local_dram,mem_load_l3_miss_retired.remote_dram -C3,7,11,15,19,23,27,31 &
     perf_pid=$!;
     sleep 1;
     kill -9 ${perf_pid};
